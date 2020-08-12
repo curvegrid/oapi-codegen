647357e8551d72cf92eab80a8fbe2e980558a2c1
Natsu Kagami <natsukagami@gmail.com>
2020-08-12 16:01:50 -0400

    Add generate server script

---
+++ generate-server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+go run cmd/oapi-codegen/oapi-codegen.go -package spec -o ../multibaas/server/app/web/api/spec/generated.go -generate types,server /home/natsukagami/curvegrid/multibaas-openapi/reference/MultiBaas-API.v1.yaml
