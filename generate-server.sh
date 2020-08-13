5698f709307fa9998a8db57878568f63d4b8b8fb
Natsu Kagami <natsukagami@gmail.com>
2020-08-13 17:02:27 -0400

    Run goimports after generation

--- generate-server.sh
+++ generate-server.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+set -e
+
 go run cmd/oapi-codegen/oapi-codegen.go -package spec -o ../multibaas/server/app/web/api/spec/generated.go -generate types,server /home/natsukagami/curvegrid/multibaas-openapi/reference/MultiBaas-API.v1.yaml
+
+goimports -w ../multibaas/server
