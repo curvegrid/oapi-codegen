3bc4b94787a7f3ba9dcf84b434a47981b083cba2
Natsu Kagami <natsukagami@gmail.com>
2020-08-13 17:24:42 -0400

    Sort enum list

--- generate-server.sh
+++ generate-server.sh
@@ -4,4 +4,5 @@ set -e
 
 go run cmd/oapi-codegen/oapi-codegen.go -package spec -o ../multibaas/server/app/web/api/spec/generated.go -generate types,server /home/natsukagami/curvegrid/multibaas-openapi/reference/MultiBaas-API.v1.yaml
 
-goimports -w ../multibaas/server
+cd ../multibaas/server
+goimports -w .
